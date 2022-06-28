175f04ace782ed6f6da45532dd3643aad62dc9fd
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-28 17:46:21 +0100

    Route Details page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Details from './components/pages/Details';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route path="/team/:name" element={<Details />} />
     </Routes>
   );
 }
