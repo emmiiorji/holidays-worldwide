c3f1211546cce56e42363ad85de040554b1ea0d6
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-28 17:42:46 +0100

    Initialize Details page

---
+++ src/components/pages/Details.js
@@ -0,0 +1,11 @@
+import { FaAngleLeft } from 'react-icons/fa';
+
+const Details = () => (
+  <section>
+    <button type="button">
+      <FaAngleLeft style={{ fontSize: '30px' }} />
+    </button>
+  </section>
+);
+
+export default Details;
