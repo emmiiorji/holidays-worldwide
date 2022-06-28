d95c1a42ba92644b9d169420699fd073621fa500
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-28 17:45:28 +0100

    Route Home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <></>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+    </Routes>
   );
 }
 
