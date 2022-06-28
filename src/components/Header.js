3e50baf24850262e4b1455e83f057fd4b5f1d19b
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-28 17:37:34 +0100

    Initialize Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { FiMenu } from 'react-icons/fi';
+
+const Header = () => (
+  <header>
+    <div>
+      <FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />
+    </div>
+  </header>
+);
+
+export default Header;
