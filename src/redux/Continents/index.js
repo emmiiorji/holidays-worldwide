7d0c96cddbd83e17cf6f95621dbf313c9eb1e2b6
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-29 21:52:20 +0100

    Add action creator for Continents reducer

--- src/redux/Continents/index.js
+++ src/redux/Continents/index.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { continents } from '../../services/APIs';
+
 // Actions
 const Actions = {
   LOAD: 'worldwide-holidays-app/continents/LOAD',
@@ -5,3 +8,25 @@ const Actions = {
 
 // Reducer
 const stateInit = {};
+
+const reducer = (state = stateInit, action = {}) => {
+  // Write reducer
+};
+
+// Action creators
+export const loadContinents = () => async (dispatch) => {
+  const response = await axios.get(continents());
+  const continentsContries = {};
+  response.data.forEach((continent) => {
+    continentsContries[continent.continent] = continent.countries;
+  });
+
+  if (response.status === 200) {
+    dispatch({
+      type: Actions.LOAD,
+      payLoad: continentsContries,
+    });
+  }
+};
+
+export default reducer;
