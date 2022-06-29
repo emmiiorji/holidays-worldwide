a75c85cf5d1b52befa50094da021274835cf38fc
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-29 21:44:38 +0100

    Add action creator for Holidays reducer

--- src/redux/Holidays/index.js
+++ src/redux/Holidays/index.js
@@ -1,6 +1,23 @@
+import axios from 'axios';
+import { countries } from '../../services/APIs';
+
 // Actions
 const Actions = {
   LOAD: 'worldwide-holidays-app/holidays/LOAD',
 };
 
 const stateInit = [];
+
+// Action Creators
+export const loadCountries = () => async (dispatch) => {
+  const response = await axios.get(countries());
+  if (response.status === 200) {
+    const payLoad = response.data.response.countries;
+    dispatch({
+      type: Actions.LOAD,
+      payLoad,
+    });
+  }
+};
+
+export default reducer;
