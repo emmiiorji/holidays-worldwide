d58381104b1c71294dd832db321e9958b0285c1b
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-01 12:26:41 +0100

    Modify Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import { FiMenu } from 'react-icons/fi';
+import './css/header.css';
 
 const Header = () => (
   <header>
-    <div>
-      <FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />
+    <div className="hamburger">
+      <FiMenu style={{ color: '#fff', width: '30px', height: '30px' }} />
+    </div>
+    <div className="headerMain">
+      <h1 className="heading">Worldwide Holidays</h1>
     </div>
   </header>
 );
