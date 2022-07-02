dc42633975b7fccc1a79b1e44b147cb293e264e3
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-02 08:58:07 +0100

    Create markup for SelectYearPopup component

---
+++ src/components/SelectYearPopup.js
@@ -0,0 +1,24 @@
+import { FaTimes } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import Datetime from 'react-datetime';
+import 'react-datetime/css/react-datetime.css';
+
+const SelectYearPopup = ({ display, hidePopup, countrySelected }) => {
+  (
+    <div className="popupOverlay" style={{ display }}>
+      <div className="popupContainer">
+        <FaTimes onClick={() => hidePopup()} className="closeBu" />
+        {/* <div className="selectYearPopup"> */}
+        <form className="selectYearPopup">
+          <p>Select Year</p>
+          <Datetime dateFormat="YYYY" timeFormat={false} closeOnSelect />
+          {/* <input type="date" id="year" onChange={handleChange} /> */}
+          <input type="submit" value="See Holidays" />
+        </form>
+        {/* </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default SelectYearPopup;
