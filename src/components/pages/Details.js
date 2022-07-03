795510c185948fc1b4bcec206f134d61c411e5bb
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-03 10:26:18 +0100

    Read redux store within Holiday Details component

--- src/components/pages/Details.js
+++ src/components/pages/Details.js
@@ -1,11 +1,16 @@
 import { VscChevronLeft } from 'react-icons/vsc';
+import { useSelector } from 'react-redux/es/exports';
 
-const Details = () => (
-  <section>
-    <button type="button">
-      <VscChevronLeft style={{ fontSize: '30px' }} />
-    </button>
-  </section>
-);
+const Details = () => {
+  const countries = useSelector((state) => state.countries);
+  console.log(countries);
+  return (
+    <section>
+      <button type="button">
+        <VscChevronLeft style={{ fontSize: '30px' }} />
+      </button>
+    </section>
+  );
+};
 
 export default Details;
