9447f7934f738bc1d02e2dac658178eed1bac366
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-04 11:11:23 +0100

    Create function to calculate total holidays

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,6 +5,10 @@ const Home = () => {
   const state = useSelector((state) => state);
   const { countries, continents } = state;
 
+  const getTotalHolidays = (countries) => Object.values(countries).reduce((acc, country) => (
+    { total: acc.total + country.total_holidays }
+  ), { total: 0 }).total.toLocaleString();
+
   return (
     <>
       <Header />
