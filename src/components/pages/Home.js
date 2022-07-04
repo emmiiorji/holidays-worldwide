d95995b3cb4749ffa50f1305d259b275b56d4413
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-04 10:58:37 +0100

    Read redux store within Home page component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,19 @@
+import { useSelector } from 'react-redux/es/exports';
 import Header from '../Header';
 
-const Home = () => (
-  <>
-    <Header />
-    <section>
-      <h1>Welcome</h1>
-    </section>
-  </>
-);
+const Home = () => {
+  const state = useSelector((state) => state);
+  const { countries, continents } = state;
+
+  return (
+    <>
+      <Header />
+      <section>
+        <h1>Welcome</h1>
+      </section>
+    </>
+
+  );
+};
 
 export default Home;
