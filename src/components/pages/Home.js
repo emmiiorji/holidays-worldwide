13300d93d7edce9c21a266138110f9fa4188d263
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-04 15:36:52 +0100

    Add section to show countries

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,6 @@
 import { useSelector } from 'react-redux/es/exports';
+import { FaRegArrowAltCircleDown } from 'react-icons/fa';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Header from '../Header';
 import Stat from '../Stat';
@@ -6,6 +8,8 @@ import '../css/home.css';
 
 const Home = () => {
   const state = useSelector((state) => state);
+  const [displayPopup, setDisplayPopup] = useState('none');
+  const [countrySelected, setCountrySelected] = useState('');
   const { countries, continents } = state;
 
   const getTotalHolidays = (countries) => Object.values(countries).reduce((acc, country) => (
@@ -31,6 +35,33 @@ const Home = () => {
               const { name, value } = statToShow;
               return <Stat key={uuidv4()} name={name} value={value} />;
             })}
+          </section>
+          <section className="showCountries">
+            <div className="filterBar">
+              <FaRegArrowAltCircleDown className="dropdown" />
+            </div>
+            <div className="countriesContainer">
+              {Object.values(countries).map((country) => {
+                const altFlagImage = 'https://res.cloudinary.com/emmii/image/upload/v1656765109/general/inserted-red-color-sticker-label-with-word-unavailable1_vavx1n.jpg';
+                return (
+                  <button
+                    className="showCountry"
+                    type="button"
+                    key={uuidv4()}
+                    onClick={() => { setDisplayPopup('flex'); setCountrySelected(country['iso-3166']); }}
+                  >
+                    <div>
+                      <img className="countryFlag" src={`${country.flag || altFlagImage}`} alt={`${country.country_name}'s flag`} />
+                      <p>
+                        {country.country_name}
+                        :&nbsp;
+                        <span style={{ color: 'yellow' }}>{country.total_holidays}</span>
+                      </p>
+                    </div>
+                  </button>
+                );
+              })}
+            </div>
           </section>
         </main>
       </div>
