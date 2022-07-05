7c102a4df760314a5d247c92bb88ec384038554a
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-05 15:32:21 +0100

    Make the back button on details page functional

--- src/components/pages/HolidaysDetails.js
+++ src/components/pages/HolidaysDetails.js
@@ -1,12 +1,14 @@
 import { VscChevronLeft } from 'react-icons/vsc';
 import { useSelector } from 'react-redux/es/exports';
+import { useNavigate } from 'react-router-dom';
 
 const Details = () => {
+  const navigate = useNavigate();
   const countries = useSelector((state) => state.countries);
   console.log(countries);
   return (
     <section>
-      <button type="button">
+      <button type="button" onClick={() => navigate(-1)}>
         <VscChevronLeft style={{ fontSize: '30px' }} />
       </button>
     </section>
