d903e9f57ca39978a2c6710739108daf347cb325
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-05 16:31:35 +0100

    Add 404 error page to routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import Details from './components/pages/HolidaysDetails';
+import NotFound from './components/pages/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route path="/holidays/:country" element={<Details />} />
+      <Route path="/notfound" element={<NotFound />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
