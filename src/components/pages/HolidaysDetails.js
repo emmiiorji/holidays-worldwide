aa4708e3178625e418abbb6fa9d7b726d628ac36
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-06 21:41:09 +0100

    Navigate to error page if parameter contains an invalid country

--- src/components/pages/HolidaysDetails.js
+++ src/components/pages/HolidaysDetails.js
@@ -1,11 +1,33 @@
 import { VscChevronLeft } from 'react-icons/vsc';
 import { useSelector } from 'react-redux/es/exports';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getObjectLength } from './Home';
+import SelectYearPopup from '../SelectYearPopup';
 
 const HolidaysDetails = () => {
   const navigate = useNavigate();
   const countries = useSelector((state) => state.countries);
   console.log(countries);
+  const { countryName } = useParams();
+  let countryCode = '';
+  const [country, ...rest] = Object.values(countries).filter((selectedCountry) => {
+    if (selectedCountry.country_name === countryName) {
+      countryCode = selectedCountry['iso-3166'];
+      return true;
+    }
+    return false;
+  });
+  rest.length = 0;
+
+  useEffect(() => {
+    if (getObjectLength(countries)) {
+      if (!country) {
+        navigate('/404');
+      }
+    }
+  });
+
   return (
     <section>
       <button type="button" onClick={() => navigate(-1)}>
