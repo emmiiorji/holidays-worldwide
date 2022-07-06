8451eb43f222a9cde3f119113b04cb271dbbb310
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-06 22:01:21 +0100

    Complete HolidaysDetails page adding Holidays component to it

--- src/components/pages/HolidaysDetails.js
+++ src/components/pages/HolidaysDetails.js
@@ -1,19 +1,20 @@
 import { VscChevronLeft } from 'react-icons/vsc';
 import { useSelector } from 'react-redux/es/exports';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { getObjectLength } from './Home';
+import Holidays from '../Holidays';
 import SelectYearPopup from '../SelectYearPopup';
+import headerStyle from '../css/header.module.css';
+import '../css/holidaysDetails.css';
 
 const HolidaysDetails = () => {
   const navigate = useNavigate();
   const countries = useSelector((state) => state.countries);
-  console.log(countries);
-  const [displayPopup, setDisplayPopup] = useState('flex');
-  const { countryName } = useParams();
+  const { countryName, year } = useParams();
   let countryCode = '';
   const [country, ...rest] = Object.values(countries).filter((selectedCountry) => {
-    if (selectedCountry.country_name === countryName) {
+    if (selectedCountry.country_name.toLowerCase() === countryName.toLocaleLowerCase()) {
       countryCode = selectedCountry['iso-3166'];
       return true;
     }
@@ -29,24 +30,49 @@ const HolidaysDetails = () => {
     }
   });
 
-  if (getObjectLength(countries) && !country.holidays) {
-    // Ask to enter year if a valid country url was visited but didn't come through the home page
+  if (getObjectLength(countries)) {
+    if (!country.holidays || !country.holidays[year]) {
+      // Ask to enter year if a valid country url was visited but didn't come through the home page
+      return (
+        <SelectYearPopup
+          display="flex"
+          hidePopup={() => navigate('/')}
+          countrySelected={countryCode}
+        />
+      );
+    }
+  }
+
+  if (country) {
+    const { holidays } = country;
     return (
-      <SelectYearPopup
-        display={displayPopup}
-        hidePopup={() => setDisplayPopup('none')}
-        countrySelected={countryCode}
-      />
+      <div className="wrapper">
+        <div className="overlay">
+          <header className={headerStyle.header}>
+            <div className="previousPage">
+              <VscChevronLeft style={{ fontSize: '30px', cursor: 'pointer' }} onClick={() => navigate(-1)} />
+            </div>
+            <div className={headerStyle.headerMain}>
+              <h1 className={headerStyle.heading}>{country.country_name}</h1>
+            </div>
+          </header>
+          <main className="mainContainer">
+            <div className="contentHeader">
+              <h2>
+                Holidays breakdown&nbsp; - &nbsp;
+                {year}
+              </h2>
+            </div>
+            <div className="holidaysContainer">
+              <Holidays holidays={holidays[year]} />
+            </div>
+          </main>
+        </div>
+      </div>
     );
   }
 
-  return (
-    <section>
-      <button type="button" onClick={() => navigate(-1)}>
-        <VscChevronLeft style={{ fontSize: '30px' }} />
-      </button>
-    </section>
-  );
+  return <></>;
 };
 
 export default HolidaysDetails;
