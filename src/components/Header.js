cb7409ef3d96b3966636ff5e4082961c50c6e614
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-06 21:49:48 +0100

    Convert Header.css to a modular css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import { FiMenu } from 'react-icons/fi';
-import './css/header.css';
+import styles from './css/header.module.css';
 
 const Header = () => (
-  <header>
-    <div className="hamburger">
+  <header className={styles.header}>
+    <div className={styles.hamburger}>
       <FiMenu style={{ color: '#fff', width: '30px', height: '30px' }} />
     </div>
-    <div className="headerMain">
-      <h1 className="heading">Worldwide Holidays</h1>
+    <div className={styles.headerMain}>
+      <h1 className={styles.heading}>Worldwide Holidays</h1>
     </div>
   </header>
 );
