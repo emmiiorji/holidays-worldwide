c272458b9279c88c5b9b3d953b222e73904dbb12
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-07 04:26:24 +0100

    Use the Dropdown component with the Home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,10 @@
 import { useSelector } from 'react-redux/es/exports';
-import { FaRegArrowAltCircleDown } from 'react-icons/fa';
+// import { FaRegArrowAltCircleDown } from 'react-icons/fa';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Header from '../Header';
 import Stat from '../Stat';
+import Dropdown from '../Dropdown';
 import '../css/home.css';
 import SelectYearPopup from '../SelectYearPopup';
 
@@ -14,6 +15,7 @@ const Home = () => {
   const [displayPopup, setDisplayPopup] = useState('none');
   const [countrySelected, setCountrySelected] = useState('');
   const { countries, continents } = state;
+  const continentNames = Object.keys(continents);
 
   const getTotalHolidays = (countries) => Object.values(countries).reduce((acc, country) => (
     { total: acc.total + country.total_holidays }
@@ -40,7 +42,7 @@ const Home = () => {
           <section className="showCountries">
             <div className="filterBar">
               <input type="text" className="searchBox" placeholder="Search Country" />
-              <FaRegArrowAltCircleDown className="dropdown" />
+              <Dropdown options={continentNames} />
             </div>
             <div className="countriesContainer">
               {Object.values(countries).map((country) => {
