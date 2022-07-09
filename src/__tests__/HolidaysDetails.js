d9bd2d1549b7a0c1029a6e0ce98ca77ee0f93458
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-09 20:19:59 +0100

    Add snapshot test for the HolidaysDetails component

---
+++ src/__tests__/HolidaysDetails.js
@@ -0,0 +1,16 @@
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../redux/configureStore';
+import HolidaysDetails from '../components/pages/HolidaysDetails';
+
+it('matches snapshot', () => {
+  const tree = renderer.create(
+    <Provider store={store}>
+      <Router>
+        <HolidaysDetails />
+      </Router>
+    </Provider>,
+  );
+  expect(tree).toMatchSnapshot();
+});
